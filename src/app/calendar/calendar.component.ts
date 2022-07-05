86df20bce0318c54dcf987c21d3da51cda26f79a
YuriiKuznietsov <kela256@gmail.com>
2022-07-05 15:22:15 +0300

    remove console log

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -36,7 +36,6 @@ export class CalendarComponent implements OnInit {
       this.monthNumber = 1;
       this.year++;
     }
-    console.log(`year ${this.year}, month ${this.monthNumber}`)
 
     this.setMonthDays(this.calendarCreator.getMonth(this.monthNumber, this.year));
   }
@@ -48,7 +47,6 @@ export class CalendarComponent implements OnInit {
       this.monthNumber = 12;
       this.year--;
     }
-    console.log(`year ${this.year}, month ${this.monthNumber}`)
 
     this.setMonthDays(this.calendarCreator.getMonth(this.monthNumber, this.year));
   }
